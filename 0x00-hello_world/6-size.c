14cf033c0d340637d2adb4ac2b8ebe1f6b733d25
Michael Gift <simiyugift@outlook.com>
2023-06-15 16:01:13 -0700

    Add C program to show size of various data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float sizeFloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(sizeChar));
-	printf("Size of a int: %lu byte(s)\n", sizeof(sizeInt));
+	printf("Size of an int: %lu byte(s)\n", sizeof(sizeInt));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(sizeLongInt));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(sizeLongLongInt));
 	printf("Size of a float: %lu byte(s)\n", sizeof(sizeFloat));
