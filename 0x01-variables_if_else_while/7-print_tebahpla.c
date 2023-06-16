bb6b3f5204c0f9144ee21a31e0e3b29992d27988
Michael Gift <simiyugift@outlook.com>
2023-06-16 15:59:10 -0700

    Add z-a printer using C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print lowercase letters in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
