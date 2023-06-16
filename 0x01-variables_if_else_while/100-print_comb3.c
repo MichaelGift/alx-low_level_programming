e1f7e92258c87db748702438015924f9ee90c669
Michael Gift <simiyugift@outlook.com>
2023-06-16 16:50:48 -0700

    Add two digit combiner

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print the smallest combination 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a);
+			putchar(b);
+
+			if (a != '8' || b != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
