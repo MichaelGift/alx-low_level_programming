f7a56a8e0332bfb89294dd06b35fca3ee62c9478
Michael Gift <simiyugift@outlook.com>
2023-06-16 15:18:46 -0700

    Add C  a-z printer with an exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
