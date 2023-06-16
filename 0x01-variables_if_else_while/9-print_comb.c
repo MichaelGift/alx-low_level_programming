9de61a90075a0d821c03d9d6cb71ab96f65ad7f4
Michael Gift <simiyugift@outlook.com>
2023-06-16 16:28:34 -0700

    Add 0-9 printer with commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print 0-9 with commas and spaces
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
