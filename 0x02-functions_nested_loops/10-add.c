939e8f442505b91b393564af4d9cc32904da74d5
Michael Gift <simiyugift@outlook.com>
2023-06-20 16:06:41 -0700

    Add program to add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers and return the result
+ * @a: first int param
+ * @b: second int param
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	int result = 0;
+
+	result = a + b;
+	return (result);
+}
