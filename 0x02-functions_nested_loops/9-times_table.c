ffbf7b435b0b3cbc79f48a1f26bd299779477f75
Michael Gift <simiyugift@outlook.com>
2023-06-20 15:55:58 -0700

    Add program to create 9x9 mult table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,40 +5,40 @@
  */
 void times_table(void)
 {
-	int x = 0; /* factor */
-	int y; /* count  */
-	int z; /* computed value */
+	int multiplicand = 0;
+	int multiplier;
+	int result;
 
-	while (x < 10)
+	while (multiplicand < 10)
 	{
-		y = 0;
-		while (y < 10)
+		multiplier = 0;
+		while (multiplier < 10)
 		{
-			z = x * y;
+			result = multiplicand * multiplier;
 
-			if (z > 9)
+			if (result > 9)
 			{
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
+				_putchar(result / 10  + '0');
+				_putchar(result % 10 + '0');
 			}
-			else if (y != 0)
+			else if (multiplier != 0)
 			{
 				_putchar(' ');
-				_putchar(z + '0');
+				_putchar(result + '0');
 			}
 			else
 			{
-				_putchar(z + '0');
+				_putchar(result + '0');
 			}
 
-			if (y != 9)
+			if (multiplier != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			y++;
+			multiplier++;
 		}
 		_putchar('\n');
-		x++;
+		multiplicand++;
 	}
 }
