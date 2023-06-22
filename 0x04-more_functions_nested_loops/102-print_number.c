5bfd2365088bfa2002c5ddc2bcb1f841d2738a41
Michael Gift <simiyugift@outlook.com>
2023-06-22 15:56:34 -0700

    Add program to print ints using _putchar

---
+++ 0x04-more_functions_nested_loops/102-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
+{
+	unsigned int count;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	count = 1;
+
+	while (n/count > 9)
+		count *= 10;
+
+	while (count >= 1)
+	{
+		_putchar((n / count) + '0');
+		n %= count;
+		count /= 10;
+	}
+}
