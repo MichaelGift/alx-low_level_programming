168a4f457e42eeba972b890a06c696f6992dcbc0
Michael Gift <simiyugift@outlook.com>
2023-06-22 10:26:25 -0700

    Add a test for the positive negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test function that prints if int is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
