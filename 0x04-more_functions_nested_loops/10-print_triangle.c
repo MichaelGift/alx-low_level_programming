7ee169603253016e9c8de91f62a999533de21f7c
Michael Gift <simiyugift@outlook.com>
2023-06-22 06:09:50 -0700

    Add complex triangle printer

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - using # print a right angle triangle based on input
+ * @size: param to determine triangle size
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int count, row;
+
+	if (size <= 0)
+		_putchar('\n');
+	for  (row = 0; row < size; row++)
+	{
+		for (count = 0; count < size; count++)
+		{
+			int spacing = size - row -1;
+
+			if (count < spacing)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
