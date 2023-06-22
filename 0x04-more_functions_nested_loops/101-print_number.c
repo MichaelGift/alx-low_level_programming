322961efa8f720bfd5a13518c53ca4e8f395d33a
Michael Gift <simiyugift@outlook.com>
2023-06-22 08:48:38 -0700

    Add program to print ints using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,31 +6,17 @@
  */
 void print_number(int n)
 {
+	int p;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n >= 1000)
+	if (n / 10 != 0)
 	{
-		_putchar((n / 1000 % 10) + '0');
-		_putchar((n / 100 % 10) + '0');
-		_putchar((n / 10 % 10) + '0');
-		_putchar((n % 10) + '0');
+		p = n / 10;
+		print_number(p);
 	}
-	else if (n >= 100)
-	{
-		_putchar((n / 100 % 10) + '0');
-		_putchar((n / 10 % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 10)
-	{
-		_putchar((n / 10 % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar(n + '0');
-	}
+	_putchar(n % 10 + '0');
 }
