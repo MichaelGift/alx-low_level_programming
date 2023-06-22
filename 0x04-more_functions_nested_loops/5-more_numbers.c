c7aa260366fd29e44fd5c416c54133359419a97b
Michael Gift <simiyugift@outlook.com>
2023-06-22 03:43:09 -0700

    Add program to print 0-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - print 0-14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int count = 1;
+	int num;
+
+	while (count <= 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+			else
+			{
+				_putchar(num + '0');
+			}
+		}
+		_putchar('\n');
+		count++;
+	}
+}
