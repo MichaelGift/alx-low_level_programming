50cc50d71a9cc1407424cdc9c055c474872a781c
Michael Gift <simiyugift@outlook.com>
2023-06-26 06:19:11 -0700

    Add a function to print every other index in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,9 @@ void puts2(char *str)
 {
 	int c = 0;
 
-	while (str[c] != '\0')
+	for (c = 0; str[c] != '\0' || str[c + 1] != '\0'; c += 2)
 	{
 		_putchar(str[c]);
-		c += 2;
 	}
 	_putchar('\n');
 }
