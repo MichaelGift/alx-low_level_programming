79eca263c62825642eb3882c6c7ea75fae8b367e
Michael Gift <simiyugift@outlook.com>
2023-07-17 07:21:06 -0700

    Add a dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This repo contains practice for creating structsuct in C and their operations
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog entity
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: Defines the attributes of dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
