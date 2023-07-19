391a2670940123e82bfd83a9d4e41d397a499db5
Michael Gift <simiyugift@outlook.com>
2023-07-19 08:34:17 -0700

    Add a function to print the opcode for the main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,20 +13,20 @@ int main(int argc, char **argv)
 	int num_bytes, index;
 	char *main_func = (char *)main;
 
-	if (argc != 2)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
 	num_bytes = atoi(argv[1]);
 
-	if (num_bytes <= 0)
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	for (index = 0; index < num_bytes; index++)
+	for (index = 0; index < num_bytes - 1; index++)
 		printf("%02hhx ", main_func[index]);
 	printf("\n");
 
