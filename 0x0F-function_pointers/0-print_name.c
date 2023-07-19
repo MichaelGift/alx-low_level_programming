9d13a0abbd974a4d9d84a35b41a44590749a7c1f
Michael Gift <simiyugift@outlook.com>
2023-07-19 05:29:45 -0700

    Add a function to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - Prints name
+ * @name: the strin to be printed
+ * @f: pointer to the print function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This Repo contains practice projects for function pointer in C
