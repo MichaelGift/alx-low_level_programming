01a6770d17eb9637dff739dba2c3d986178b9b8c
Michael Gift <simiyugift@outlook.com>
2023-08-01 22:01:15 +0000

    Add a function to create a node a the start of the linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,10 +13,12 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *new_node;
+
 	if (head == NULL)
 		return (NULL);
 
-	listint_t *new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
 	if (!new_node)
 		return (NULL);
@@ -24,7 +26,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
-
-	return (head);
+	
+	return (*head);
 
 }
