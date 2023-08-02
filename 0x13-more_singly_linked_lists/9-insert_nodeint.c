82e1fcb0922798b679dae01a15ed6ee0a0e5602d
Michael Gift <simiyugift@outlook.com>
2023-08-02 00:27:53 +0000

    Add a function that insert and node at index n
    A Certain Scientific Index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	next_ptr = *head;
 	previous_ptr = NULL;
-	for (i = 0;i < idx; i++)
+	for (i = 0; i < idx; i++)
 	{
 		if (!next_ptr)
 			return (NULL);
