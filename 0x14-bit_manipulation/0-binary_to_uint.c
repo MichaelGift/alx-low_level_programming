ef1e2833852d5f4358585a0ac92b8755fbd414d9
Michael Gift <simiyugift@outlook.com>
2023-08-03 21:48:12 +0000

    Add a function to convert binary to unsigned integers

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, int_n;
-	int_n = 0;
 
+	int_n = 0;
 	i = 0;
+
 	if (!b)
 		return (0);
+
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+
 		int_n <<= 1;
+
 		if (b[i] == '1')
 			int_n += 1;
+
 		i++;
 	}
 	return (int_n);
