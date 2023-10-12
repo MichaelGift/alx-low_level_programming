fb9656d0fecb462eb5fd74ca56fe39266e1ed6f7
Michael Gift <simiyugift@outlook.com>
2023-10-12 23:20:47 +0000

    Update README.md

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print a double linked list
+ * @h: header of double linked list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n_c = 0;
+	int i_p = 0;
+
+	for (n_c = 0; h != NULL; n_c++)
+	{
+		if (h->prev != NULL && i_p == 0)
+		{
+			h = h->prev;
+			continue;
+		}
+		else if (h->prev == NULL)
+		{
+			i_p = 1;
+			n_c= 0;
+		}
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
